673cb18ccd5eea627691051313444ca47448d7a2
Igniii <josepignasiferrergarriga@hotmail.com>
2026-06-05 22:30:08 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
+        boolean loop = true;
+        while (loop = true) {
+            System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        // Prints the "<command>: command not found" message
-        System.out.println(command + ": command not found");
+            Scanner scanner = new Scanner(System.in);
+            String command = scanner.nextLine();
+            // Prints the "<command>: command not found" message
+            System.out.println(command + ": command not found");
+        }
     }
 }
