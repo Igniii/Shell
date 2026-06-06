cfd9706ff63e1fa6c55835dbfbade56a7d1f4486
Igniii <josepignasiferrergarriga@hotmail.com>
2026-06-07 00:53:27 +0200

    codecrafters submit [skip ci]

--- src/main/java/Builtin.java
+++ src/main/java/Builtin.java
@@ -1,13 +1,13 @@
 public class Builtin {
 
+    static String input = Main.command;
+
     static void exit() {
         System.exit(0);
     }
 
     static void echo() {
-        String input = Main.command;
-        int index = input.indexOf("");
-
-        System.out.println(input.substring(index));
+        int index = input.indexOf(" ");
+        System.out.println(input.substring(index + 1));
     }
 }
