57e758b4db848dc1a6881b0c58d153e71d240b6d
Igniii <josepignasiferrergarriga@hotmail.com>
2026-06-07 00:44:14 +0200

    codecrafters submit [skip ci]

---
+++ src/main/java/Builtin.java
@@ -0,0 +1,13 @@
+public class Builtin {
+
+    static void exit() {
+        System.exit(0);
+    }
+
+    static void echo() {
+        String input = Main.command;
+        int index = input.indexOf(" ");
+
+        System.out.println(input.substring(index));
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String command;
+
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        // Implementando un bucle REPL para que el shell se ejecute constantemente
-        boolean repl = true;
-        while (repl) {
-            System.out.print("$ ");
 
-            String command = scanner.nextLine();
+        // Implementando un bucle REPL para que el shell se ejecute constantemente
+        while (true) {
+            System.out.print("$ ");
+            command = scanner.nextLine();
             // Lógica para implementar el comando builtin "exit". Usamos equals para comparar
             // dos Strings y el builtin exit para salir del programa.
             if (command.equals("exit")) {
-                System.exit(0);
+                Builtin.exit();
+            } else if (command.startsWith("echo ")) {
+                Builtin.echo();
             } else {
                 System.out.println(command + ": command not found");
             }
