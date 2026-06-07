2a691cacf815d138c0104f35554090dba88b45e9
Igniii <josepignasiferrergarriga@hotmail.com>
2026-06-07 21:56:34 +0200

    codecrafters submit [skip ci]

--- src/main/java/Builtin.java
+++ src/main/java/Builtin.java
@@ -1,3 +1,7 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Builtin {
 
     static void exit() {
@@ -12,12 +16,36 @@ public class Builtin {
     static void type() {
         if (Main.command.equals("type echo")) {
             System.out.println("echo is a shell builtin");
-        } else if (Main.command.equals("type exit")) {
-            System.out.println("exit is a shell builtin");
-        } else if (Main.command.equals("type type")) {
-            System.out.println("type is a shell builtin");
-        } else {
-            System.out.println(Main.command.substring(5) + ": not found");
+            return;
         }
+
+        if (Main.command.equals("type exit")) {
+            System.out.println("exit is a shell builtin");
+            return;
+        }
+
+        if (Main.command.equals("type type")) {
+            System.out.println("type is a shell builtin");
+            return;
+        }
+
+        String commandName = Main.command.substring(5);
+        String pathEnv = System.getenv("PATH");
+        String separator = System.getProperty("path.separator");
+
+        for (String dir : pathEnv.split(
+            java.util.regex.Pattern.quote(separator)
+        )) {
+            Path executable = Paths.get(dir, commandName);
+
+            if (Files.exists(executable) && Files.isExecutable(executable)) {
+                System.out.println(
+                    commandName + " is " + executable.toAbsolutePath()
+                );
+                return;
+            }
+        }
+
+        System.out.println(commandName + ": not found");
     }
 }
